9f024f6ac9e682304f4ac5792beae6998f07f2fd
Rémi Latrille <remi.latrille@gmail.com>
2021-01-10 23:16:43 +0100

    Add Social networks to theme customizer

--- config/customizer/register.php
+++ config/customizer/register.php
@@ -13,13 +13,12 @@ function neptune_customizer_options( $wp_customize ) {
 	$wp_customize->add_control(
 		new WP_Customize_Color_Control(
 			$wp_customize,
-			'neptune_custom_background_color',
+			'neptune_control_background_color',
 			array(
 				'label'      => __( 'Background Color', 'neptune' ),
 				'section'    => 'colors',
 				'settings'   => 'neptune_background_color',
-				'description' => __('Choisissez la couleur de fond du site', 'neptune'),
-				'selector' => "#neptune_bg_color"
+				'description' => __('Please select a background color', 'neptune'),
 			)
 		)
 	);
@@ -27,4 +26,120 @@ function neptune_customizer_options( $wp_customize ) {
 	/**-----------------------------------------
 	 *  Social networks
 	 ---------------------------------------- */
+
+	$wp_customize->add_section('neptune_section_social_networks', array(
+		'title'          => __('Social Networks', 'neptune'),
+		'description' 	 => __("Please add your social networks links. Leave empty if you don't want the social network's icon to appear.", 'neptune'),
+		'priority'       => 81,
+	));
+
+	// Facebook
+	$wp_customize->add_setting('neptune_social_facebook', array(
+		'transport' => 'refresh',
+	));
+
+	$wp_customize->add_control(
+		new WP_Customize_Control(
+			$wp_customize,
+			'neptune_control_social_facebook',
+			array(
+				'label'      => __( 'Facebook Link', 'neptune' ),
+				//'description' => __('Please enter your Facebook account link', 'neptune'),
+				'input_attrs' => array(
+					'placeholder' => __( 'https://', 'neptune' ),
+				),
+				'section'    => 'neptune_section_social_networks',
+				'settings'   => 'neptune_social_facebook',
+				'type' => 'text',
+			)
+		)
+	);
+
+	// Twitter
+	$wp_customize->add_setting('neptune_social_twitter', array(
+		'transport' => 'refresh',
+	));
+
+	$wp_customize->add_control(
+		new WP_Customize_Control(
+			$wp_customize,
+			'neptune_control_social_twitter',
+			array(
+				'label'      => __( 'Twitter Link', 'neptune' ),
+				//'description' => __('Please enter your Twitter account link', 'neptune'),
+				'input_attrs' => array(
+					'placeholder' => __( 'https://', 'neptune' ),
+				),
+				'section'    => 'neptune_section_social_networks',
+				'settings'   => 'neptune_social_twitter',
+				'type' => 'text',
+			)
+		)
+	);
+
+	// Instagram
+	$wp_customize->add_setting('neptune_social_instagram', array(
+		'transport' => 'refresh',
+	));
+
+	$wp_customize->add_control(
+		new WP_Customize_Control(
+			$wp_customize,
+			'neptune_control_social_instagram',
+			array(
+				'label'      => __( 'Instagram Link', 'neptune' ),
+				//'description' => __('Please enter your instagram account link', 'neptune'),
+				'input_attrs' => array(
+					'placeholder' => __( 'https://', 'neptune' ),
+				),
+				'section'    => 'neptune_section_social_networks',
+				'settings'   => 'neptune_social_instagram',
+				'type' => 'text',
+			)
+		)
+	);
+
+	// Instagram
+	$wp_customize->add_setting('neptune_social_tiktok', array(
+		'transport' => 'refresh',
+	));
+
+	$wp_customize->add_control(
+		new WP_Customize_Control(
+			$wp_customize,
+			'neptune_control_social_tiktok',
+			array(
+				'label'      => __( 'Tiktok Link', 'neptune' ),
+				//'description' => __('Please enter your tiktok account link', 'neptune'),
+				'input_attrs' => array(
+					'placeholder' => __( 'https://', 'neptune' ),
+				),
+				'section'    => 'neptune_section_social_networks',
+				'settings'   => 'neptune_social_tiktok',
+				'type' => 'text',
+			)
+		)
+	);
+
+	// LinkedIn
+	$wp_customize->add_setting('neptune_social_linkedin', array(
+		'transport' => 'refresh',
+	));
+
+	$wp_customize->add_control(
+		new WP_Customize_Control(
+			$wp_customize,
+			'neptune_control_social_linkedin',
+			array(
+				'label'      => __( 'LinkedIn Link', 'neptune' ),
+				//'description' => __('Please enter your linkedin account link', 'neptune'),
+				'input_attrs' => array(
+					'placeholder' => __( 'https://'),
+				),
+				'section'    => 'neptune_section_social_networks',
+				'settings'   => 'neptune_social_linkedin',
+				'type' => 'text',
+			)
+		)
+	);
 }
