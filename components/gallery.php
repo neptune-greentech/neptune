2fea3fd42fc7bb7f825df48df74165950236dcaa
Mathieu Daix <mathieudaixpro@gmail.com>
2021-01-28 13:54:16 +0100

    add gallery component [WIP]

---
+++ components/gallery.php
@@ -0,0 +1,24 @@
+<!-- <section class="gallery">
+
+    <div class="container">
+        <h2>View the gallery</h2>
+    </div>
+
+    <div class="slider js-slider">
+        <div class="slider_inner js-slider__inner">
+            <div class="slide js-slide">
+                <div class="slide_bg" style="background-image: url('http://localhost:8888/neptune/wp-content/uploads/2021/01/image.jpg')"></div>
+            </div>
+            <div class="slide js-slide">
+                <div class="slide_bg" style="background-image: url('http://localhost:8888/neptune/wp-content/uploads/2021/01/image.jpg')"></div>
+            </div>
+            <div class="slide js-slide">
+                <div class="slide_bg" style="background-image: url('http://localhost:8888/neptune/wp-content/uploads/2021/01/image.jpg')"></div>
+            </div>
+            <div class="slide js-slide">
+                <div class="slide_bg" style="background-image: url('http://localhost:8888/neptune/wp-content/uploads/2021/01/image.jpg')"></div>
+            </div>
+        </div>
+    </div>
+
+</section> -->
