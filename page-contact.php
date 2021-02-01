a312b03f5c2efe01568932d8b72064216b072619
Mathieu Daix <mathieudaixpro@gmail.com>
2021-02-01 11:26:57 +0100

    add place component

---
+++ page-contact.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Template Name: page-contact
+ */
+
+?>
+
+<?php get_header(); ?>
+
+<?php include_once locate_template('/components/hero-classic.php'); ?>
+
+<?php include_once locate_template('/components/place.php'); ?>
+
+<?php get_footer(); ?>
