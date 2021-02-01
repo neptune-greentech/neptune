a448abd728107d479b673ad0817959c4084f5ec3
Mathieu Daix <mathieudaixpro@gmail.com>
2021-02-01 10:10:32 +0100

    add footer component

--- config/menus.php
+++ config/menus.php
@@ -2,7 +2,8 @@
 
 function neptune_menus() {
 	register_nav_menus( array(
-		'main_menu' => __('Main Menu')
+		'main_menu' => __('Main Menu'),
+		'secondary_menu' => __('Secondary Menu')
 	) );
 }
 add_action( 'after_setup_theme', 'neptune_menus');
--- footer.php
+++ footer.php
@@ -1,3 +1,47 @@
+    <footer class="footer bg">
+        <div class="container footer_container">
+        
+            <a class="footer_logo" href="<?= home_url(); ?>">
+                <img src="http://localhost:8888/neptune/wp-content/uploads/2021/01/logo-neptune.png" alt="Logo Neptune.">
+            </a>
+
+            <?php 
+            wp_nav_menu(
+                array( 
+                    'menu' => 'main_menu',
+                    'container' => 'div',
+                    'container_class' => 'footer_menu',
+                    'items_wrap' => '<ul>%3$s</ul>'
+                )
+            );
+            ?>
+
+            <ul class="footer_social">
+                <li>
+                    <a href="#">
+                        <img src="http://localhost:8888/neptune/wp-content/uploads/2021/02/icon-facebook.png" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="http://localhost:8888/neptune/wp-content/uploads/2021/02/icon-instagram.png" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="http://localhost:8888/neptune/wp-content/uploads/2021/01/icon-linkedin.png" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="http://localhost:8888/neptune/wp-content/uploads/2021/01/icon-twitter.png" alt="">
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+    </footer>
+
 <?php wp_footer(); ?>
 
 </body>
