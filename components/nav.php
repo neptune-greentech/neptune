e9facabce25aba31ce9ac59d12f161292c24a68c
Mathieu Daix <mathieudaixpro@gmail.com>
2023-09-01 16:42:31 +0200

    update dynamisation of logo

--- components/nav.php
+++ components/nav.php
@@ -1,9 +1,19 @@
 <nav class="nav">
     <div class="container nav_container">
 
-        <a class="nav_logo" href="<?= home_url(); ?>">
-            <img src="<?php echo get_template_directory_uri() . '/dist/images/logo-neptune.png'; ?>" alt="Logo Neptune.">
-        </a>
+        <?php 
+        $custom_logo_id = get_theme_mod( 'custom_logo' );
+        $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+        if ( has_custom_logo() ) {
+        ?>
+            <a class="nav_logo" href="<?= home_url(); ?>">
+                <img src="<?= esc_url($logo[0]); ?>" alt="Logo.">
+            </a>
+        <?php
+        } else {
+            echo '<p>' . get_bloginfo('name') . '</p>';
+        }
+        ?>
 
         <div class="nav_icon">
             <span></span>
--- config/theme_support.php
+++ config/theme_support.php
@@ -1,3 +1,5 @@
 <?php
 
 add_theme_support( 'post-thumbnails', array( 'post' ));
+
+add_theme_support( 'custom-logo' );
